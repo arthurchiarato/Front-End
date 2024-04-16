925aa015c326c40373bae56249a8945a87cf0af8
arthurchiarato <160291638+arthurchiarato@users.noreply.github.com>
2024-04-16 08:25:39 -0300

    Add files via upload

---
+++ Pedido tóxico/script.js
@@ -0,0 +1,14 @@
+function sim() {
+    alert("Parabéns, agora você está em um relacionamento sério!");
+    
+}
+
+function não() {
+    var intel = prompt("Você tem certeza? Sim ou não");
+    while (intel != "Não") {
+        var intel = prompt("Você tem certeza mesmo? Sim ou não?");
+        
+    }
+    alert("Parabéns, agora você está em um relacionamento sério!");
+
+}
